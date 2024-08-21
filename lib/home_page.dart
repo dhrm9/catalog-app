d726247daa63a44e980b3d3fd0147f781acc7067
dhrm9 <dharamendrasingh11@gmail.com>
2024-08-21 22:39:25 +0530

    day1

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        home: Material(
+          child: Center(
+            child: Text(
+              "hello",
+              
+              style: TextStyle(color: Colors.red,fontSize: 52),
+              ),
+          ),
+        ),
+    );
+  }
+}
